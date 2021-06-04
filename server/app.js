e9a2ccdb94349003e8512d5c6f186a5c8e697da9
Can Agriboz <agriboz@gmail.com>
2021-06-04 14:12:46 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import middlewares from './middlewares.js'
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 666
-
+// change port to safe one could be > 8080, 3000 
 app.set('port', port)
 
 middlewares(app)
